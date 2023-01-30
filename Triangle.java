3ac8f57898c9ba899df664289f83bb3776bd035c
Yongchao <Yongchao.ma@vcsu.edu>
2023-01-30 00:05:35 -0600

    fix

--- Triangle.java
+++ Triangle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Triangle
 {
     public static void main(String[] args)
@@ -29,7 +31,7 @@ public class Triangle
             }//if
  
             //To check to see if it is an Isosceles triangle (2 of the same size and 1 different)
-            else if ((side1 == side2) & (side2 = side3) || (side2 == side3) & (side3!= side1))
+            else if ((side1 == side2) & (side2 == side3) || (side2 == side3) & (side3!= side1))
             {
                 System.out.print("\n RESULT: An Isosceles Triangle.");
             }//else if
